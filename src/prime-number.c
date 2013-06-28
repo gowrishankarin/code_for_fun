75070c06a54486ef5a7a875339c33768bf886447
Gowri Shankar <gowrishankar_m@hotmail.com>
2013-06-28 23:22:48 +0530

    Prime finder algorithm implemented for C++ module.

--- src/prime-number.c
+++ src/prime-number.c
@@ -1,23 +1,29 @@
+#include <iostream>
+#include <math.h>
+#include <unistd.h>
+#include <ctime>
+#include <sys/time.h>
 
+using namespace std;
 
-int prime(int lastNumber) {
-	int primesSize = (lastNumber -1)/2;
-	char* isPrime = new char[primeSize+1];
-	for(int i = 0; i <= primesSize; i++) {
+long long prime(long long lastNumber) {
+	long primesSize = (lastNumber -1)/2;
+	char* isPrime = new char[primesSize+1];
+	for(long long i = 0; i <= primesSize; i++) {
 		isPrime[i] = 1;
 	}
 
-	for (int i = 3; i*i <= lastNumber; i+=2) {
-		if(isPrime[i/1]) {
-			for(int j = i*i; j <= lastNumber; j+=2*i) {
+	for (long long i = 3; i*i <= lastNumber; i+=2) {
+		if(isPrime[i/2]) {
+			for(long long j = i*i; j <= lastNumber; j+=2*i) {
 				isPrime[j/2] = 0;
 			}
 		}
 	}
 
 
-	int found = lastNumber >= 2 ? 1 : 0;
-	for(int i = 1; i <= primesSize; i++) {
+	long long found = lastNumber >= 2 ? 1 : 0;
+	for(long long i = 1; i <= primesSize; i++) {
 		found += isPrime[i];
 	}
 
@@ -26,7 +32,21 @@ int prime(int lastNumber) {
 };
 
 
+int main() {
 
+	for(int i = 10; i <= 30; i++) {
+		
+		timeval startTime, endTime;
+		
+		gettimeofday(&startTime, 0);
+		long long primeCount = prime(pow(2, i));
+		gettimeofday(&endTime, 0);
+		std::cout << "Prime Count: " << primeCount << " Duration: " << (endTime.tv_usec - startTime.tv_usec)/1000 << std::endl;
+		
+	}
+}
+
+/*
 int perfCalc {
 
 	Object perfData = [];
@@ -51,3 +71,4 @@ int perfCalc {
 }
 perfCalc();
 
+*/
