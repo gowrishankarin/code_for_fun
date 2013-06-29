fbd2e27f866093121bd9176bb763537cfd621628
Gowri Shankar <gowrishankar_m@hotmail.com>
2013-06-29 20:33:06 +0530

    Learning Python, first sensible commit.

--- src/prime-number.py
+++ src/prime-number.py
@@ -1,59 +1,19 @@
-#include <stdio.h>
 
-int prime(int lastNumber) {
-	int primesSize = (lastNumber -1)/2;
-	char* isPrime = new char[primeSize+1];
-	for(int i = 0; i <= primesSize; i++) {
-		isPrime[i] = 1;
-	}
+def prime(int lastNumber):
+	primesSize = (lastNumber -1)/2
+	char* isPrime = new char[primeSize+1]
+	for i = 0; i <= primesSize; i++) 
+		isPrime[i] = 1
+	
 
-	for (int i = 3; i*i <= lastNumber; i+=2) {
-		if(isPrime[i/1]) {
-			for(int j = i*i; j <= lastNumber; j+=2*i) {
-				isPrime[j/2] = 0;
-			}
-		}
-	}
+	for (i = 3; i*i <= lastNumber; i+=2) 
+		if(isPrime[i/1]) 
+			for(j = i*i; j <= lastNumber; j+=2*i) 
+				isPrime[j/2] = 0
 
+	found = lastNumber >= 2 ? 1 : 0
+	for(i = 1; i <= primesSize; i++) 
+		found += isPrime[i]
 
-	int found = lastNumber >= 2 ? 1 : 0;
-	for(int i = 1; i <= primesSize; i++) {
-		found += isPrime[i];
-	}
-
-	delete [] isPrime;
-	return found;
-};
-
-
-int main() {
-
-	prime(10);
-}
-
-/*
-int perfCalc {
-
-	Object perfData = [];
-
-
-	for(int i=10; i <= 27; i++) {
-
-		int startTime = new Date();
-		int primeCount = prime(Math.pow(2, i));
-		int endTime = new Date();
-		int data = new Object();
-
-		data.duration = endTime.getTime() - startTime.getTime();
-		data.range = Math.pow(2, i);
-		data.primeCount = primeCount;
-		perfData.push(data);
-		console.log(data);
-	}
-	console.log(perfData);
-	fs.writeFileSync("perfData.txt", JSON.stringify(perfData));
-
-}
-perfCalc();
-
-*/
+def main():
+	prime(10)
