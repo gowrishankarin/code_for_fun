aee34fd6e8e23c655640d7b6ea1aab5b8f0e2819
Gowri Shankar <gowrishankar_m@hotmail.com>
2013-06-29 23:05:17 +0530

    More modification to python code

--- src/prime-number.py
+++ src/prime-number.py
@@ -1,19 +1,19 @@
 
 def prime(int lastNumber):
-	primesSize = (lastNumber -1)/2
-	char* isPrime = new char[primeSize+1]
+	primesSize = (lastNumber -1)/2;
 	for i = 0; i <= primesSize; i++) 
 		isPrime[i] = 1
-	
 
 	for (i = 3; i*i <= lastNumber; i+=2) 
-		if(isPrime[i/1]) 
+		if isPrime[i/1] 
 			for(j = i*i; j <= lastNumber; j+=2*i) 
 				isPrime[j/2] = 0
 
 	found = lastNumber >= 2 ? 1 : 0
 	for(i = 1; i <= primesSize; i++) 
 		found += isPrime[i]
+	
+
 
 def main():
 	prime(10)
