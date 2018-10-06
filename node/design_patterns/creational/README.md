d91b6ebb8b9835c46da8115674e9f6eb8045e318
Gowri Shankar <shankar.chari@gmail.com>
2018-10-06 10:14:54 +0530

    Prototype - Intent

--- node/design_patterns/creational/README.md
+++ node/design_patterns/creational/README.md
@@ -7,3 +7,8 @@ Ensure a class only has one instance, and provide a global point of access to it
 start - Prototype and Problem Definition
 second - Conventional Implemention of Singleton in Node
 final - Node's own Singleton Implementation
+
+### Prototype
+Intent:
+Specify the kinds of object to create using prototypical instance, and create new
+objects by copying this prototype.
---
+++ node/design_patterns/creational/prototype/start/index.js
@@ -0,0 +1,15 @@
+var Shopper = require('./Shopper');
+
+var alex =  new Shopper('Alex Banks');
+alex.addItemToList('Camping Knife');
+alex.addItemToList('Tent');
+alex.addItemToList('Backpack');
+alex.addItemToList('Map');
+alex.addItemToList('Slingshot');
+
+var eve =  new Shopper('Eve Porcello');
+alex.addItemToList('Camping Knife');
+alex.addItemToList('Tent');
+alex.addItemToList('Backpack');
+alex.addItemToList('Map');
+alex.addItemToList('Reading Light');
