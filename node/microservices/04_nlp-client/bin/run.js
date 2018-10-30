60e65d451290126379932da107789f397127a439
Gowri Shankar <shankar.chari@gmail.com>
2018-10-30 13:22:53 +0530

    NODE: Microservices, NLP Client

---
+++ node/microservices/04_nlp-client/bin/run.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const slackClient = require('../server/slackClient');
+const service = require('../server/service');
+const http = require('http');
+
+const server = http.createServer(service);
+
+const witToken = 'XTJWRO65TFUDGBVBPPD4TYUVCOZFSOHK';
+const witClient = require('../server/witClient')(witToken);
+
+const slackToken = 'xoxb-464822094500-466054506327-3GNScD7iLgLMDY5AT9nwc5T4';
+const slackLogLevel = 'info';
+
+const rtm = slackClient.init(slackToken, slackLogLevel, witClient);
+rtm.start();
+
+server.listen(3000);
+
+server.on('listening', function() {
+    console.log(`MICROSERVICE is listening on ${server.address().port} in ${service.get('env')} mode.`)
+});
---
+++ node/microservices/04_nlp-client/server/slackClient.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const RTMClient = require('@slack/client').RTMClient;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+let rtm = null;
+let nlp = null;
+
+function handleOnAuthenticated(rtmStartData) {
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+}
+
+function handleOnMessage(message) {
+    nlp.ask(message.text, (err, res) => {
+        if(err) {
+            console.log('ASK Error: ' + err);
+            return;
+        }
+
+        if(!res.intent) {
+            return rtm.sendMessage(
+                "Sorry, I do not know what you are talking about", 
+                message.channel,
+                function messageSent() {
+
+                }
+            );
+        } else if(res.intent[0].value == 'time' && res.location) {
+            console.log(JSON.stringify(res.location));
+            return rtm.sendMessage(
+                `I don't yet know the time in ${res.location[0].value}`, 
+                message.channel,
+                function messageSent() {
+                    
+                }
+            );
+        } else {
+            return rtm.sendMessage(
+                `Sorry, I don't yet know what you are talking`, 
+                message.channel,
+                function messageSent() {
+
+                }
+            );
+        }
+
+        rtm.sendMessage('Sorry, I did not understand', message.channel, function messageSent() {
+
+        });
+    });
+
+
+}
+
+function addAuthenticatedHandler(rtm, handler) {
+    // rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
+    rtm.on('authenticated', handler);
+}
+
+module.exports.init = function slackClient(token, logLevel, nlpClient) {
+    rtm = new RTMClient(token, {
+        logLevel: logLevel
+    });
+
+    nlp = nlpClient;
+
+    addAuthenticatedHandler(rtm, handleOnAuthenticated);
+    rtm.on('message',  handleOnMessage);
+
+    return rtm;
+}
+
