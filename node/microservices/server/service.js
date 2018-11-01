f1a73dc688a2a9f1f283a4c50b5f56df4a4c839b
Gowri Shankar <shankar.chari@gmail.com>
2018-11-01 15:09:32 +0530

    NODE: Microservices, Service registery ip and port

--- node/microservices/server/service.js
+++ node/microservices/server/service.js
@@ -3,5 +3,17 @@
 const express = require('express');
 const service = express();
 
+service.put('/service/:intent/:port', (req, res, next) => {
+    const serviceIntent = req.params.intent;
+    const servicePort = req.params.port;
+
+    const serviceIp = req.connection.remoteAddress.includes('::') ?
+        `[${req.connection.remoteAddress}]` : req.connection.remoteAddress;
+
+        res.json({
+            result: `${serviceIntent} at ${serviceIp}:${servicePort}`
+        });
+});
+
 module.exports = service;
 
--- node/microservices/services/ms-time/bin/run.js
+++ node/microservices/services/ms-time/bin/run.js
@@ -1,11 +1,27 @@
 'use strict';
 
+const request = require('superagent');
 const service = require('../server/service');
 const http = require('http');
 
 const server = http.createServer(service);
-server.listen(3010);
+server.listen();
 
 server.on('listening', function() {
-    console.log(`MICROSERVICE-TIME is listening on ${server.address().port} in ${service.get('env')} mode.`)
+    console.log(`MICROSERVICE-TIME is listening on ${server.address().port} in ${service.get('env')} mode.`);
+
+    const announce = () => {
+        request.put(`http://127.0.0.1:3000/service/time/${server.address().port}`, (err, res) => {
+            if(err) {
+                console.log(err);
+                console.log("Error connecting to Iris");
+                return;
+            }
+
+            console.log(res.body);
+        });
+    };
+
+    announce(); 
+    setInterval(announce, 15*1000)
 });
