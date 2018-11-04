f215db946d892d4e3f9c7320db7c9f2bd8a5b869
Gowri Shankar <shankar.chari@gmail.com>
2018-11-04 20:28:02 +0530

    FLUTTER: Tab bar navigations

--- flutter/courses/maximillian/01_Exercise/first_exercise/lib/pages/product_admin.dart
+++ flutter/courses/maximillian/01_Exercise/first_exercise/lib/pages/product_admin.dart
@@ -1,35 +1,55 @@
 import 'package:flutter/material.dart';
 
 import './products.dart';
+import './product_create.dart';
+import './product_list.dart';
 
 class ProductAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text('Choose'),
+              ),
+              ListTile(
+                title: Text('All Products'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => ProductsPage()));
+                },
+              )
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text('Products'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: 'Create Tab',
+                icon: Icon(Icons.create),
+              ),
+              Tab(
+                text: 'Add Product',
+                icon: Icon(Icons.list),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
           children: <Widget>[
-            AppBar(
-              automaticallyImplyLeading: false,
-              title: Text('Choose'),
-            ),
-            ListTile(
-              title: Text('All Products'),
-              onTap: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => ProductsPage()));
-              },
-            )
+            ProductCreatePage(),
+            ProductListPage()
           ],
         ),
-      ),
-      appBar: AppBar(
-        title: Text('Products'),
-      ),
-      body: Center(
-        child: Text('Product Admin Page'),
       ),
     );
   }
