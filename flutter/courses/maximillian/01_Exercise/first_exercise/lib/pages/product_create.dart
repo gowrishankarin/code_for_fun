58d451e0b944e705435a123b9e00feb214118b67
Gowri Shankar <shankar.chari@gmail.com>
2018-11-11 20:20:55 +0530

    FLUTTER: Navigations on Successful save

--- flutter/courses/maximillian/01_Exercise/first_exercise/lib/pages/product_create.dart
+++ flutter/courses/maximillian/01_Exercise/first_exercise/lib/pages/product_create.dart
@@ -54,8 +54,11 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
               });
             },
           ),
+          SizedBox(height: 10.0),
           RaisedButton(
             child: Text('Save'),
+            color: Theme.of(context).accentColor,
+            textColor: Colors.white,
             onPressed: () {
               final Map<String, dynamic> product = {
                 'title': titleValue,
@@ -64,6 +67,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
                 'image': 'assets/food.jpg'
               };
               widget.addProduct(product);
+              Navigator.pushReplacementNamed(context, '/');
             },
           )
         ],
