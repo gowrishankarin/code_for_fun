a3167a93f85adb23abc6094d2c105d4ea702ad66
Gowri Shankar <shankar.chari@gmail.com>
2018-11-16 23:32:33 +0530

    FLUTTER: Forms, Form field validations

--- flutter/courses/maximillian/01_Exercise/first_exercise/lib/pages/product_create.dart
+++ flutter/courses/maximillian/01_Exercise/first_exercise/lib/pages/product_create.dart
@@ -15,13 +15,24 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   String _titleValue = '';
   String _descriptionValue = '';
   double _priceValue;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget _buildTitleTextField() {
-    return TextField(
+    return TextFormField(
       decoration: InputDecoration(
         labelText: 'Product Title',
       ),
-      onChanged: (String value) {
+      validator: (String value) {
+        if(value.isEmpty) {
+          return 'Title is required';
+        }
+      },
+      // onChanged: (String value) {
+      //   setState(() {
+      //     _titleValue = value;
+      //   });
+      // },
+      onSaved: (String value) {
         setState(() {
           _titleValue = value;
         });
@@ -30,34 +41,36 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   }
 
   Widget _buildDescriptionTextField() {
-    return TextField(
+    return TextFormField(
       maxLines: 4,
       decoration: InputDecoration(
         labelText: 'Product Description',
       ),
-      onChanged: (String value) {
+      onSaved: (String value) {
         setState(() {
           _descriptionValue = value;
         });
-      },
+      }
     );
   }
 
   Widget _buildPriceTextField() {
-    return TextField(
+    return TextFormField(
       keyboardType: TextInputType.number,
       decoration: InputDecoration(
         labelText: 'Product Price',
       ),
-      onChanged: (String value) {
+      onSaved: (String value) {
         setState(() {
           _priceValue = double.parse(value);
         });
-      },
+      }
     );
   }
 
   void _submitForm() {
+    _formKey.currentState.validate();
+    _formKey.currentState.save();
     final Map<String, dynamic> product = {
       'title': _titleValue,
       'description': _descriptionValue,
@@ -76,28 +89,31 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
     return Container(
       width: targetWidth,
       margin: EdgeInsets.all(10.0),
-      child: ListView(
-        padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
-        children: <Widget>[
-          _buildTitleTextField(),
-          _buildDescriptionTextField(),
-          _buildPriceTextField(),
-          SizedBox(height: 10.0),
-          RaisedButton(
-            child: Text('Save'),
-            color: Theme.of(context).accentColor,
-            textColor: Colors.white,
-            onPressed: _submitForm,
-          )
-          // GestureDetector(
-          //   onTap: _submitForm,
-          //   child: Container(
-          //     color: Colors.green,
-          //     padding: EdgeInsets.all(5.0),
-          //     child: Text('My Button'),
-          //   ),
-          // )
-        ],
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
+          children: <Widget>[
+            _buildTitleTextField(),
+            _buildDescriptionTextField(),
+            _buildPriceTextField(),
+            SizedBox(height: 10.0),
+            RaisedButton(
+              child: Text('Save'),
+              color: Theme.of(context).accentColor,
+              textColor: Colors.white,
+              onPressed: _submitForm,
+            )
+            // GestureDetector(
+            //   onTap: _submitForm,
+            //   child: Container(
+            //     color: Colors.green,
+            //     padding: EdgeInsets.all(5.0),
+            //     child: Text('My Button'),
+            //   ),
+            // )
+          ],
+        ),
       ),
     );
   }
