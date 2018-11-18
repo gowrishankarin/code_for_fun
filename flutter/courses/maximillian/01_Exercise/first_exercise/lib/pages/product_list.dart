8a8b43a217325ff4d3a29cb3a56357cc4291e670
Gowri Shankar <shankar.chari@gmail.com>
2018-11-18 13:21:22 +0530

    FLUTTER: Dismissible and Divider widgets for ListTile

--- flutter/courses/maximillian/01_Exercise/first_exercise/lib/pages/product_list.dart
+++ flutter/courses/maximillian/01_Exercise/first_exercise/lib/pages/product_list.dart
@@ -12,24 +12,35 @@ class ProductListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          //leading: Image.asset(products[index]['image']),
-          title: Text(products[index]['title']),
-          trailing: IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (BuildContext context) {
-                    return ProductEditPage(
-                      product: products[index],
-                      updateProduct: updateProduct,
-                      productIndex: index
+        return Dismissible(
+          key: Key(products[index]['title']),
+          background: Container(color: Colors.red),
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(products[index]['image']),
+                ),
+                title: Text(products[index]['title']),
+                subtitle: Text('\$${products[index]['price'].toString()}'),
+                trailing: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return ProductEditPage(
+                              product: products[index],
+                              updateProduct: updateProduct,
+                              productIndex: index);
+                        },
+                      ),
                     );
                   },
                 ),
-              );
-            },
+              ),
+              Divider()
+            ],
           ),
         );
       },
