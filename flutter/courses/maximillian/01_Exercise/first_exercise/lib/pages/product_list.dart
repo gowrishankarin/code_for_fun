2a665a28aa5df36238489ae5ae132c3511108e6f
Gowri Shankar <shankar.chari@gmail.com>
2018-11-18 13:28:13 +0530

    FLUTTER: Dismiss Directions

--- flutter/courses/maximillian/01_Exercise/first_exercise/lib/pages/product_list.dart
+++ flutter/courses/maximillian/01_Exercise/first_exercise/lib/pages/product_list.dart
@@ -5,8 +5,9 @@ import './product_edit.dart';
 class ProductListPage extends StatelessWidget {
   final List<Map<String, dynamic>> products;
   final Function updateProduct;
+  final Function deleteProduct;
 
-  ProductListPage(this.products, this.updateProduct);
+  ProductListPage(this.products, this.updateProduct, this.deleteProduct);
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +15,16 @@ class ProductListPage extends StatelessWidget {
       itemBuilder: (BuildContext context, int index) {
         return Dismissible(
           key: Key(products[index]['title']),
+          onDismissed: (DismissDirection direction) {
+            if(direction == DismissDirection.endToStart) {
+              print('Swipe End to Start');
+              deleteProduct(index);
+            } else if(direction == DismissDirection.startToEnd){
+              print('Swipe Start to End');
+            } else {
+              print('Other Swiping');
+            }
+          },
           background: Container(color: Colors.red),
           child: Column(
             children: <Widget>[
