d3301629923f2a183c23c5f2cdfc9e6e172b2cc8
Gowri Shankar <shankar.chari@gmail.com>
2018-11-30 23:13:34 +0530

    FLUTTER: Static Map Renderer, Dispose

---
+++ flutter/courses/maximillian/01_Exercise/first_exercise/lib/widgets/form_inputs/location.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:map_view/map_view.dart';
+
+import '../helpers/ensure_visible.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LocationInputState();
+  }
+}
+
+class _LocationInputState extends State<LocationInput> {
+  final FocusNode _addressInputFocusNode = FocusNode();
+  Uri _staticMapUri;
+
+  @override
+  void initState() {
+    _addressInputFocusNode.addListener(_updateLocation);
+    getStaticMap();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _addressInputFocusNode.removeListener(_updateLocation);
+    super.dispose();
+  }
+
+  void getStaticMap() {
+    final StaticMapProvider staticMapViewProvider =
+        StaticMapProvider('');
+    final staticMapUri = staticMapViewProvider.getStaticUriWithMarkers(
+      [Marker('position', 'Position', 41.40338, 2.17403)],
+      center: Location(41.40338, 2.17403),
+      width: 500,
+      height: 300,
+      maptype: StaticMapViewType.roadmap,
+    );
+    setState(() {
+      _staticMapUri = staticMapUri;
+    });
+  }
+
+  void _updateLocation() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        EnsureVisibleWhenFocused(
+          focusNode: _addressInputFocusNode,
+          child: TextFormField(
+            focusNode: _addressInputFocusNode,
+          ),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Image.network(_staticMapUri.toString())
+      ],
+    );
+  }
+}
