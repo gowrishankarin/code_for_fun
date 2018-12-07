2d1e29605745aaa70bdbb4a8059b6524cf9e59a4
Gowri Shankar <shankar.chari@gmail.com>
2018-12-07 22:02:23 +0530

    FLUTTER: FB Admin, FB Storage, Image Receiver etc

--- flutter/courses/maximillian/01_Exercise/first_exercise/functions/index.js
+++ flutter/courses/maximillian/01_Exercise/first_exercise/functions/index.js
@@ -1,9 +1,13 @@
 const functions = require('firebase-functions');
-const cors = require('cors')({origin: true});
+const cors = require('cors')({
+    origin: true
+});
 const Busboy = require('busboy');
 const os = require('os');
 const path = require('path');
 const fs = require('fs');
+const fbAdmin = require('firebase-admin');
+const uuid = require('uuid');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -12,22 +16,43 @@ const fs = require('fs');
 //  response.send("Hello from Firebase!");
 // });
 
+const gcconfig = {
+    projectId: 'flutter-products-gs',
+    keyFilename: 'flutter-products-gs.json'
+};
+
+const gcs = require('@google-cloud/storage')(gcconfig);
+
+fbAdmin.initializeApp({
+    credential: fbAdmin.credential.cert(require('./flutter-products-gs.json'))
+});
+
 exports.storeImage = functions.https.onRequest((req, res) => {
     return cors(req, res, () => {
-        if(req.method !== 'POST')  {
-            return res.status(500).json({message: 'Not allowed.'});
-        } 
-        if(!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
-            return res.status(403).json({error: 'Unauthorized'});
+        if (req.method !== 'POST') {
+            return res.status(500).json({
+                message: 'Not allowed.'
+            });
+        }
+        if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+            return res.status(403).json({
+                error: 'Unauthorized'
+            });
         }
 
         let idToken = req.headers.authorization.split('Bearer ')[1];
 
-        const busboy = new Busboy({headers: req.headers});
+        const busboy = new Busboy({
+            headers: req.headers
+        });
         let uploadData;
         busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
             const filePath = path.join(os.tmpdir(), filename);
-            uploadData = {filePath: filePath, type: mimetype, name: filename};
+            uploadData = {
+                filePath: filePath,
+                type: mimetype,
+                name: filename
+            };
             file.pipe(fs.createWriteStream(filePath));
         });
 
@@ -36,7 +61,44 @@ exports.storeImage = functions.https.onRequest((req, res) => {
         });
 
         busboy.on('finish', () => {
-            
+            const bucket = gcs.bucket('flutter-products-gs.appspot.com');
+            const id = uuid();
+            let imagePath = 'images/' + id + '-' + uploadData.name;
+
+            if (oldImagePath) {
+                imagePath = oldImagePath;
+            }
+
+            return fbAdmin.auth().verifyIdToken(idToken)
+                .then(decodedToken => {
+                    return bucket.upload(uploadData.filePath, {
+                        uploadType: 'media',
+                        destination: imagePath,
+                        metadata: {
+                            metadata: {
+                                contentType: uploadData.type,
+                                firebaseStorageDownloadToken: id
+                            }
+                        }
+                    });
+                })
+                .then(() => {
+                    return res.statusCode(201).json({
+                        imageUrl: 'https//firebasestorage.googleapis.com/v0/b/' + 
+                            bucket.name +
+                            '/o/' + 
+                            encodeURIComponent(imagePath) + 
+                            '?alt=media&token=' + 
+                            id,
+                        imagePath: imagePath
+                    });
+                })
+                .catch(error => {
+                    return res.statusCode(401).json({
+                        error: 'Unauthorized!'
+                    });
+                });   
         });
+        return busboy.end(req.rawBody);
     });
 });
