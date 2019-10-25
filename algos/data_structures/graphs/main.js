c1dfd890cbc7076c76327a64b36bd34dda81dba4
Gowri Shankar <shankar.chari@gmail.com>
2019-10-25 21:41:37 +0530

    ALGOS: Graphs, sample representation

---
+++ algos/data_structures/graphs/main.js
@@ -0,0 +1,13 @@
+// Edge List
+const graph = [[0, 2], [2, 3], [2, 1], [1, 3]];
+
+// Adjacent List
+const graph_adj = [[2], [2, 3], [0, 1, 3], [1, 2]];
+
+// Adjacent Matrix
+const graph_mat = {
+    0: [0, 1, 0, 0],
+    1: [1, 0, 1, 1],
+    2: [1, 1, 0, 1],
+    3: [0, 1, 1, 0]
+};
