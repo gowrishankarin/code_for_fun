fb18c77343de406e52ee6d81449efc36613d8830
Gowri Shankar <shankar.chari@gmail.com>
2019-11-02 23:34:33 +0530

    JS: NestJS + MongoDB Connections Basics

--- node/nestjs/gs-info/src/app.module.ts
+++ node/nestjs/gs-info/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/product.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot("mongodb://localhost:27017/gs-info")],
   controllers: [AppController],
   providers: [AppService],
 })
--- node/nestjs/gs-info/src/products/product.service.ts
+++ node/nestjs/gs-info/src/products/product.service.ts
@@ -1,19 +1,25 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import {InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
 import { Product } from './product.model';
 
 @Injectable()
 export class ProductService {
     private products: Product[] = [];
+    private readonly productModel: Model<Product>;
 
-    consturctor() {
+    consturctor(
+        @InjectModel('Product')  productModel: Model<Product>
+    ) {
 
     }
 
-    insertProduct(title: string, desc: string, price: number): string {
-        const prodId = Math.random().toString();
-        const newProduct = new Product(prodId, title, desc, price);
-        this.products.push(newProduct);
-        return prodId;
+    async insertProduct(title: string, desc: string, price: number) {
+        const newProduct =  this.productModel({title: title, description: desc, price: price});
+        const result = await newProduct.save();
+        console.log(result);
+        return 'prodId';
     }
 
     fetchProducts() {
