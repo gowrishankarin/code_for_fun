be1cca7dfca34fcb86d641eb7ad09d853cfd4e4f
Gowri Shankar <shankar.chari@gmail.com>
2019-11-12 23:16:02 +0530

    ALGOS: Memoization basics

--- algos/problems/memoization/main.js
+++ algos/problems/memoization/main.js
@@ -4,11 +4,30 @@ function memoizedAddTo80(n) {
         return cache[n];
     } else {
         console.log('Long Time');
-        cache[n] = n + 8;
+        cache[n] = n + 80;
         return cache[n];
     }
 }
 
 console.log('1', memoizedAddTo80(5));
-console.log('1', memoizedAddTo80(5));
-console.log('1', memoizedAddTo80(15))
+console.log('2', memoizedAddTo80(5));
+console.log('3', memoizedAddTo80(15));
+
+// With closures
+
+function memoizedAddTo800() {
+    let cache = {};
+    return function(n) {
+        if(n in cache) {
+            return cache[n];
+        } else {
+            console.log('Long Time');
+            cache[n] = n + 800;
+            return cache[n];
+        }
+    }
+}
+const memized = memoizedAddTo800();
+console.log('4', memized(5));
+console.log('5', memized(5));
+console.log('6', memized(15));
