ce7a0c9b865b6a117955f732860af9957159b3a5
Gowri Shankar <shankar.chari@gmail.com>
2019-11-14 20:37:36 +0530

    ALGOS: Memoization, Looping a Fionacci

--- algos/problems/memoization/fibonacci.js
+++ algos/problems/memoization/fibonacci.js
@@ -28,10 +28,19 @@ function fibonacciMaster() {
     }
 }
 
+function fibonacciMaster2(n) {
+    let answer = [0, 1];
+    for(let i=2; i <= n; i++) {
+        answer.push(answer[i-2] + answer[i-1])
+    }
+    return answer.pop()
+}
+
 const fasterFib = fibonacciMaster();
 let n = 5;
 console.log('Slow', fibonacci(n));
 console.log('We did ' + calculations + ' calculations');
 
 console.log('DP', fasterFib(n));
+console.log('DP2', fibonacciMaster2(100))
 console.log('We did ' + calculations1 + ' calculations');
