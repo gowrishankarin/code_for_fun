dcd104a610ddacd35c46bf09a06a6cf299914b72
Gowri Shankar <shankar.chari@gmail.com>
2019-11-15 21:43:21 +0530

    ANGULAR8: Event binding

--- node/nestjs/gs-info-cli/src/app/servers/servers.component.ts
+++ node/nestjs/gs-info-cli/src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created'; 
+  serverName = '';
 
   constructor() { 
     setTimeout(() => {
@@ -21,5 +22,9 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was Create';
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+    console.log(this.serverName);
   }
 }
