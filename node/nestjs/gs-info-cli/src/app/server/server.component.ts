0b42c6ea307c9da65f80f45f28e5af6f15dd69c3
Gowri Shankar <shankar.chari@gmail.com>
2019-11-17 16:38:41 +0530

    ANGULAR8: ngStyle and ngClass directives

--- node/nestjs/gs-info-cli/src/app/server/server.component.ts
+++ node/nestjs/gs-info-cli/src/app/server/server.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
 })
 export class ServerComponent {
     private serverId: number = 10;
     private serverStatus: string = 'offline';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
+    }
 }
