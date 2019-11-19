983445081a7ce6c102980ea9509ebda2a6ea4094
Gowri Shankar <shankar.chari@gmail.com>
2019-11-19 19:45:14 +0530

    ANGULAR8: Components for Shopping Cart and Recipes

--- node/nestjs/gs-info-cli/src/app/app.module.ts
+++ node/nestjs/gs-info-cli/src/app/app.module.ts
@@ -7,14 +7,28 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
+    HeaderComponent,
     ServerComponent, 
     ServersComponent, 
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
