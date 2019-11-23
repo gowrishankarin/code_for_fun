8a0fc52d742ccc961bffea0a9403f5c6292a3b96
Gowri Shankar <shankar.chari@gmail.com>
2019-11-23 22:51:04 +0530

    ANGULAR8: Event Emitters

--- node/nestjs/gs-info-cli/src/app/cockpit/cockpit.component.ts
+++ node/nestjs/gs-info-cli/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
 
-  //serverElements = [];
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();;
+  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent:string}>();
   newServerName = '';
   newServerContent = '';
 
@@ -17,19 +18,16 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer() {
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
 
   onAddBlueprint() {
-    // this.serverElements.push({
-    //   type: 'blueprint',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // })
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
-
 }
--- node/nestjs/gs-info-cli/src/app/server-main/server-main.component.ts
+++ node/nestjs/gs-info-cli/src/app/server-main/server-main.component.ts
@@ -17,4 +17,25 @@ export class ServerMainComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  onServerAdded(serverData: {
+    serverName: string, serverContent: string
+  }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {
+    serverName: string,
+    serverContent: string
+  }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
