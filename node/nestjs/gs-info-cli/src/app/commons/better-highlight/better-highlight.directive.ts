a94a517bd025a110ad383fea023b1f041d4301e3
Gowri Shankar <shankar.chari@gmail.com>
2019-12-07 01:41:01 +0530

    ANGULAR8: Directives, HostListeners

--- node/nestjs/gs-info-cli/src/app/commons/better-highlight/better-highlight.directive.ts
+++ node/nestjs/gs-info-cli/src/app/commons/better-highlight/better-highlight.directive.ts
@@ -3,22 +3,27 @@ import {
   OnInit,
   Renderer2,
   ElementRef,
-  HostListener
+  HostListener,
+  HostBinding
 } from "@angular/core";
 
 @Directive({
   selector: "[appBetterHighlight]"
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding("style.backgroundColor") backgroundColor: string = "transparent";
+
   @HostListener("mouseenter") mouseover(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, "color", "blue");
+    this.backgroundColor = "cyan";
   }
 
   @HostListener("mouseleave") mouseleave(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, "color", "black");
+    this.backgroundColor = "transparent";
   }
   ngOnInit(): void {
-    this.renderer.setStyle(this.elRef.nativeElement, "color", "blue");
+    // this.renderer.setStyle(this.elRef.nativeElement, "color", "blue");
   }
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 }
